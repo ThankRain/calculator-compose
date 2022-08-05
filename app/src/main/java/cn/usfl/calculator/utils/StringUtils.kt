da23a7460342484a2a77146fe3a2b04c00c8486e
Xeu <thankrain@qq.com>
2022-08-05 18:06:18 +0800

    [clean] 清理代码

--- app/src/main/java/cn/usfl/calculator/utils/StringUtils.kt
+++ app/src/main/java/cn/usfl/calculator/utils/StringUtils.kt
@@ -12,18 +12,6 @@ fun CharSequence.indexOfChars(start: Int = 0, end: Int = length - 1, vararg char
     return null
 }
 
-//从Start开始快速反向查找指定的Char的位置，不存在返回null
-fun CharSequence.lastIndexOfChars(
-    start: Int = length - 1,
-    end: Int = 0,
-    chars: Array<Char>
-): Int? {
-    for (i in start downTo end) {
-        if (this[i] in chars) return i
-    }
-    return null
-}
-
 fun CharSequence.countPair(first: Char, second: Char): Pair<Int, Int> {
     var firstCounter = 0
     var secondCounter = 0
